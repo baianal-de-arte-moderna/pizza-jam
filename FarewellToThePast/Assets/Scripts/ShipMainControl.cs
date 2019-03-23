bf1169c3f31b226868e2e3690826d6dfb8784a27
Piramba <avpaganini@gmail.com>
2019-03-23 11:47:33 -0300

    Full navinha control com PAN e ROTATION e ROLL

---
+++ FarewellToThePast/Assets/Scripts/ShipMainControl.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipMainControl : MonoBehaviour
+{
+    const float LERP_SPEED = 0.1f;
+    const float DEFAULT_MOVE_SPEED = 0.2f;
+    const float DEFAULT_MANEUVER_SPEED = 1f;
+    Transform m_transform;
+    public Vector3 target_location;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_transform = GetComponent<Transform>();
+        target_location = m_transform.position;
+    }
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void Update()
+    {
+        m_transform.position = Vector3.Lerp(m_transform.position, target_location, LERP_SPEED);
+    }
+    
+    // Z-Axis functions
+    public void Accel(float speed=DEFAULT_MOVE_SPEED) {
+        target_location += m_transform.forward * speed;
+    }
+
+    public void Reverse(float speed=DEFAULT_MOVE_SPEED) {
+        target_location -= m_transform.forward * speed;
+    }
+
+    // X-Axis functions
+    public void LeftRotation(float speed=DEFAULT_MANEUVER_SPEED) {
+        m_transform.Rotate(Vector3.up * -speed);
+    }
+
+    public void RightRotation(float speed=DEFAULT_MANEUVER_SPEED) {
+        m_transform.Rotate(Vector3.up * speed);
+    }
+
+    // Y-Axis functions
+    public void UpRotation(float speed=DEFAULT_MANEUVER_SPEED) {
+        m_transform.Rotate(Vector3.right * -speed);
+    }
+
+    public void DownRotation(float speed=DEFAULT_MANEUVER_SPEED) {
+        m_transform.Rotate(Vector3.right * speed);
+    }
+}
---
+++ FarewellToThePast/Assets/Scripts/ShipPanControl.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipPanControl : MonoBehaviour
+{
+    const float LERP_SPEED = 0.1f;
+    const float ROLL_SPEED = 10f;
+    Transform mTransform;
+    Vector3 targetLocation;
+    float verticalMaxPan;
+    float horizontalMaxPan;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mTransform = GetComponent<Transform>();
+        targetLocation = mTransform.localPosition;
+
+        // 2.5f to make model stay fully within the boundaries
+        verticalMaxPan = Mathf.Abs(
+            Camera.main.transform.localPosition.z * 
+            Mathf.Tan(
+                Camera.main.fieldOfView / 2.5f * Mathf.Deg2Rad
+            )
+        );
+        horizontalMaxPan = Camera.main.aspect * verticalMaxPan;
+    }
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void Update()
+    {
+        var movementReference = mTransform.localPosition - targetLocation;
+        movementReference *= ROLL_SPEED;
+        mTransform.rotation = Quaternion.Euler(movementReference.y, movementReference.z, movementReference.x);
+
+        mTransform.localPosition = Vector3.Lerp(mTransform.localPosition, targetLocation, LERP_SPEED);
+    }
+    
+    // X-Axis functions
+    public void LeftPan(float speed) {
+        targetLocation -= Vector3.right * speed;
+        targetLocation.x = Mathf.Max(targetLocation.x, -horizontalMaxPan);
+    }
+
+    public void RightPan(float speed) {
+        targetLocation += Vector3.right * speed;
+        targetLocation.x = Mathf.Min(targetLocation.x, horizontalMaxPan);
+    }
+
+    public void LeftRoll(float speed) {
+        mTransform.Rotate(Vector3.forward * -speed);
+    }
+
+    public void RightRoll(float speed) {
+        mTransform.Rotate(Vector3.forward * speed);
+    }
+
+    // Y-Axis functions
+    public void UpPan(float speed) {
+        targetLocation += Vector3.up * speed;
+        targetLocation.y = Mathf.Min(targetLocation.y, verticalMaxPan);
+    }
+
+    public void DownPan(float speed) {
+        targetLocation -= Vector3.up * speed;
+        targetLocation.y = Mathf.Max(targetLocation.y, -verticalMaxPan);
+    }
+
+    public void UpRoll(float speed) {
+        mTransform.Rotate(Vector3.right * -speed);
+    }
+
+    public void DownRoll(float speed) {
+        mTransform.Rotate(Vector3.right * speed);
+    }
+}
