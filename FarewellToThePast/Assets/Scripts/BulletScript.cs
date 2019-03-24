300c148f1e3d2938f34aeb98a3df5154b46312d1
Vinicius Uzeda <vruzeda@gmail.com>
2019-03-24 09:32:50 -0300

    Adjust bullet collision checking

--- FarewellToThePast/Assets/Scripts/BulletScript.cs
+++ FarewellToThePast/Assets/Scripts/BulletScript.cs
@@ -31,14 +31,8 @@ public class BulletScript : MonoBehaviour
         }
     }
 
-    /// <summary>
-    /// OnCollisionEnter is called when this collider/rigidbody has begun
-    /// touching another rigidbody/collider.
-    /// </summary>
-    /// <param name="other">The Collision data associated with this collision.</param>
-    void OnCollisionEnter(Collision other)
-    {
-        if (!other.gameObject.CompareTag("Player")) {
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Player")) {
             Debug.Log("Bullet hit");
             EndShot();
         }
