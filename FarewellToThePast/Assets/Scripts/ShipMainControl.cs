e64dbd5e6bd51137780d649d45d5aa6afcf6a6cd
Vinicius Uzeda <vruzeda@gmail.com>
2019-03-24 08:54:00 -0300

    Only enable propulsion particles when the thruster are on

--- FarewellToThePast/Assets/Scripts/ShipMainControl.cs
+++ FarewellToThePast/Assets/Scripts/ShipMainControl.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,11 +9,13 @@ public class ShipMainControl : MonoBehaviour
     const float MAX_THRUSTER_SPEED = 0.50f;
     const float FORWARD_SPEED_LIMIT = 100f;
     const float BACKWARD_SPEED_LIMIT = -FORWARD_SPEED_LIMIT;
+    const float THRUSTER_ON_THRESHOLD = MAX_THRUSTER_SPEED * 0.1f;
 
     const float DEFAULT_MANEUVER_SPEED = 1f;
 
     new Transform transform;
     new Rigidbody rigidbody;
+    ParticleSystem[] particles;
 
     float forwardVelocity;
     float forwardThrusterSpeed;
@@ -23,38 +26,59 @@ public class ShipMainControl : MonoBehaviour
     {
         transform = GetComponent<Transform>();
         rigidbody = GetComponent<Rigidbody>();
+        particles = GetComponentsInChildren<ParticleSystem>();
         forwardVelocity = 0f;
         forwardThrusterSpeed = 0f;
         backwardThrusterSpeed = 0f;
     }
 
-    private void Update()
-    {
-        if (Mathf.Approximately(forwardVelocity, 0))
-        {
-
-        }
-        else
-        {
-
-        }
-    }
-
     // Z-Axis functions
     public void Accel(float acceleration = DEFAULT_ACCELERATION) {
-        forwardThrusterSpeed = Mathf.Min(forwardThrusterSpeed + acceleration, MAX_THRUSTER_SPEED);
-        backwardThrusterSpeed = 0f;
+        ChangeSpeedHandlingParticles(() => {
+            forwardThrusterSpeed = Mathf.Min(forwardThrusterSpeed + acceleration, MAX_THRUSTER_SPEED);
+            backwardThrusterSpeed = 0f;
 
-        forwardVelocity = Mathf.Min(forwardVelocity + forwardThrusterSpeed, FORWARD_SPEED_LIMIT);
-        rigidbody.velocity = transform.forward * forwardVelocity;
+            forwardVelocity = Mathf.Min(forwardVelocity + forwardThrusterSpeed, FORWARD_SPEED_LIMIT);
+            rigidbody.velocity = transform.forward * forwardVelocity;
+        });
     }
 
     public void Reverse(float acceleration = DEFAULT_ACCELERATION) {
-        forwardThrusterSpeed = 0f;
-        backwardThrusterSpeed = Mathf.Min(backwardThrusterSpeed + acceleration, MAX_THRUSTER_SPEED);
+        ChangeSpeedHandlingParticles(() => {
+            forwardThrusterSpeed = 0f;
+            backwardThrusterSpeed = Mathf.Min(backwardThrusterSpeed + acceleration, MAX_THRUSTER_SPEED);
 
-        forwardVelocity = Mathf.Max(forwardVelocity - backwardThrusterSpeed, BACKWARD_SPEED_LIMIT);
-        rigidbody.velocity = transform.forward * forwardVelocity;
+            forwardVelocity = Mathf.Max(forwardVelocity - backwardThrusterSpeed, BACKWARD_SPEED_LIMIT);
+            rigidbody.velocity = transform.forward * forwardVelocity;
+        });
+    }
+
+    private void ChangeSpeedHandlingParticles(Action changeSpeed) {
+        bool wasThrusterOn = IsThrusterOn();
+
+        changeSpeed();
+
+        bool isThrusterOn = IsThrusterOn();
+
+        if (wasThrusterOn != isThrusterOn) {
+            ToggleParticles(isThrusterOn);
+        }
+    }
+
+    private bool IsThrusterOn() {
+        bool isForwardThrusterOn = forwardThrusterSpeed > THRUSTER_ON_THRESHOLD;
+        bool isBackwardThrusterOn = backwardThrusterSpeed > THRUSTER_ON_THRESHOLD;
+        return isForwardThrusterOn || isBackwardThrusterOn;
+    }
+
+    private void ToggleParticles(bool enable) {
+        foreach (ParticleSystem particle in particles) {
+            if (enable) {
+                particle.Play();
+            } else {
+                particle.Pause();
+            }
+        }
     }
 
     // X-Axis functions
