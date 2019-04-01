7d6c149103caae2fefe0e3a6d714234cc1b52ab5
Piramba <avpaganini@gmail.com>
2019-03-31 23:36:06 -0300

    Input ready

--- FarewellToThePast/Assets/TesteArcade/Scripts/ArcadeInput.cs
+++ FarewellToThePast/Assets/TesteArcade/Scripts/ArcadeInput.cs
@@ -6,10 +6,14 @@ public class ArcadeInput : MonoBehaviour
 {
     public TextAsset joypadConfiguration;
     public Dictionary<string, KeyCode> inputCommands;
+    public Dictionary<string, string> inputAxis;
+    public Dictionary<string, char> inputDirections;
     // Start is called before the first frame update
     void Start()
     {
         inputCommands = new Dictionary<string, KeyCode>();
+        inputAxis = new Dictionary<string, string>();
+        inputDirections = new Dictionary<string, char>();
         // Clear commands
         ClearCommands();
 
@@ -22,40 +26,49 @@ public class ArcadeInput : MonoBehaviour
 
         var joypadConfigurationLines = joypadConfiguration.text.Split('\n');
         foreach (string line in joypadConfigurationLines) {
-            var joypadConfigurationWords = line.Split('-', '=');
-            if (joypadConfigurationWords.Length != 3 ||
-                joypadConfigurationWords[0] != StaticConstants.PLAYER1_ID ||
-                ! inputCommands.ContainsKey(joypadConfigurationWords[1])) {
+            var joypadConfigurationWords = line.Split('_', '=');
+            if (joypadConfigurationWords.Length != 3) continue;
+
+            var p = joypadConfigurationWords[0].Trim();
+            var c = joypadConfigurationWords[1].Trim();
+            var i = joypadConfigurationWords[2].Trim();
+            if (p != StaticConstants.PLAYER1_ID ||
+                !inputCommands.ContainsKey(c)) {
                     continue;
                 }
-                inputCommands[joypadConfigurationWords[1]] = StaticConstants.StringToKey(joypadConfigurationWords[2]);
+
+                inputCommands[c] = StaticConstants.StringToKey(i);
+                if (inputCommands[c] == KeyCode.None) {
+                    inputAxis[c] = i.Substring(0, i.Length - 1);
+                    inputDirections[c] = i[i.Length - 1];
+                }
         }
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (Input.GetKey(inputCommands[StaticConstants.CMD_FORWARD])) {
+        if (ProccessInput(StaticConstants.CMD_FORWARD)) {
             Debug.Log("Accelerating");
         }
 
-        if (Input.GetKey(inputCommands[StaticConstants.CMD_BRAKE])) {
+        if (ProccessInput(StaticConstants.CMD_BRAKE)) {
             Debug.Log("Braking");
         }
 
-        if (Input.GetKey(inputCommands[StaticConstants.CMD_LEFT])) {
+        if (ProccessInput(StaticConstants.CMD_LEFT)) {
             Debug.Log("Left");
         }
 
-        if (Input.GetKey(inputCommands[StaticConstants.CMD_RIGHT])) {
+        if (ProccessInput(StaticConstants.CMD_RIGHT)) {
             Debug.Log("Right");
         }
 
-        if (Input.GetKey(inputCommands[StaticConstants.CMD_UP])) {
+        if (ProccessInput(StaticConstants.CMD_UP)) {
             Debug.Log("Up");
         }
 
-        if (Input.GetKey(inputCommands[StaticConstants.CMD_DOWN])) {
+        if (ProccessInput(StaticConstants.CMD_DOWN)) {
             Debug.Log("Down");
         }
     }
@@ -63,6 +76,32 @@ public class ArcadeInput : MonoBehaviour
     void ClearCommands() {
         foreach (string command in StaticConstants.CMD_LIST) {
             inputCommands[command] = KeyCode.None;
+            inputAxis[command] = null;
+            inputDirections[command] = '\0';
         }
+    }
+
+    bool ProccessButton(KeyCode button) {
+
+        return false;
+    }
+
+    bool ProccessAxis(string axis, char direction) {
+        return false;
+    }
+
+    bool ProccessInput(string cmd) 
+    {
+        if (inputAxis[cmd] != null)
+        {
+            var axisState = Input.GetAxis(inputAxis[cmd]);
+            if (axisState == 0)
+                return false;
+            else if (inputDirections[cmd].Equals('+'))
+                return (axisState > 0);
+            else
+                return (axisState < 0);
+        }
+        return Input.GetKey(inputCommands[cmd]);
     }
 }
